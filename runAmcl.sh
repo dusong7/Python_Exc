287c4b6b9e2a302b2689b12c6dffddb46b26de66
dusong7 <dusong7@hotmail.com>
2017-02-23 10:10:59 +0800

    Create runAmcl.sh

---
+++ runAmcl.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+##gnome-terminal -x bash -c "ls;exec bash"&&gnome-terminal -x bash -c "ls;exec bash"&&gnome-terminal -x bash -c "ls;exec bash"&&
+gnome-terminal -x bash -c "roslaunch turtlebot_gazebo turtlebot_world.launch;exec bash"
+sleep 10
+gnome-terminal -x bash -c "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/admini/map6.yaml;exec bash" 
+sleep 5
+gnome-terminal -x bash -c "roslaunch turtlebot_rviz_launchers view_navigation.launch;exec bash"
