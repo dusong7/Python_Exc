5223c6a5897797525d30f2d04912abcc8da15cb5
dusong7 <dusong7@hotmail.com>
2017-04-18 16:40:25 +0800

    Update numpy_exc11.py

--- numpy_exc11.py
+++ numpy_exc11.py
@@ -35,13 +35,24 @@ print(x.view(matrix))
 import mpl_toolkits.mplot3d as mp3d
 import matplotlib.pyplot as plt
 
-x = np.linspace(0,6*np.pi,600)
-z = x.copy()
-y = np.sin(x)
-x=np.cos(x)
-fig = plt.figure()
-ax = mp3d.Axes3D(fig)
-ax.plot(x, y, zs=z)
-ax.view_init(15,45)
-plt.show()
-
+# x = np.linspace(0,6*np.pi,600)
+# z = x.copy()
+# y = np.sin(x)
+# x=np.cos(x)
+# fig = plt.figure()
+# ax = mp3d.Axes3D(fig)
+# ax.plot(x, y, zs=z)
+# ax.view_init(15,45)
+# # plt.show()
+# 
+# list = [1,3,4,5]
+# list1 = [13,33,43,52]
+# plt.plot(list)
+# plt.plot(list1)
+# plt.show()
+import time as tm
+#
+#
+# for i in range(100):
+#     print(tm.clock())
+#     tm.sleep(1)
