b461062f52e1ea6928e29a335b6519222a9fb5af
dusong7 <dusong7@hotmail.com>
2017-04-19 10:04:25 +0800

    Update numpy_exc11.py

--- numpy_exc11.py
+++ numpy_exc11.py
@@ -44,7 +44,7 @@ import matplotlib.pyplot as plt
 # ax.plot(x, y, zs=z)
 # ax.view_init(15,45)
 # # plt.show()
-# 
+#
 # list = [1,3,4,5]
 # list1 = [13,33,43,52]
 # plt.plot(list)
@@ -56,3 +56,72 @@ import time as tm
 # for i in range(100):
 #     print(tm.clock())
 #     tm.sleep(1)
+
+x = random.randn(4,3)
+print(shape(x))
+x.shape= 6,-1
+
+print(x)
+
+# from time import ctime
+# from time import sleep
+# import threading
+#
+# def music(func):
+#     for i in range(2):
+#         print("This is music %s_%s"%(func,ctime()))
+#         sleep(1)
+#
+#
+# def movie(func):
+#     for i in range(2):
+#         print("This is movie %s_%s" % (func, ctime()))
+#         sleep(5)
+#
+# threads= []
+#
+# t1 = threading.Thread(target=music,args=("Love_",))
+# threads.append(t1)
+#
+# t2 = threading.Thread(target=movie,args=("theSpeed_",))
+# threads.append(t2)
+#
+# def run():
+#     for t in threads:
+#         t.setDaemon(True)
+#         t.start()
+#
+#     print("All over_")
+#
+# if __name__ == "__main__":
+#     run()
+
+import threading
+from time import ctime,sleep
+
+
+def music(func):
+    for i in range(2):
+        print ("I was listening to %s. %s" %(func,ctime()))
+        sleep(1)
+
+def move(func):
+    for i in range(2):
+        print ("I was at the %s! %s" %(func,ctime()))
+        sleep(5)
+
+threads = []
+t1 = threading.Thread(target=music,args=(u'爱情买卖',))
+threads.append(t1)
+t2 = threading.Thread(target=move,args=(u'阿凡达',))
+threads.append(t2)
+
+def run1():
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    t.join()
+    print ("all over %s" %ctime())
+
+if __name__ == '__main__':
+    run1()
