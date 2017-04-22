0dc541f9fc8aa6e95f52b5fff054a350b33e992f
dusong7 <dusong7@hotmail.com>
2017-04-22 17:21:58 +0800

    Create runCmd.cpp

---
+++ runCmd.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <qmessagebox.h>
+#include <stdlib.h>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_ShellRun_clicked()
+{
+    QMessageBox::about(NULL,"","runCommand");
+    system("gnome-terminal -e runCommand");
+}
+
+void MainWindow::on_keyBoardRun_clicked()
+{
+    QString showMsg = "299";
+
+    QMessageBox::about(NULL,"About",showMsg);
+}
+
+void MainWindow::on_AMClRun_clicked()
+{
+    QMessageBox::aboutQt(NULL,"AboutMessage");
+}
