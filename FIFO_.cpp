77a78d6bab723f186b45a9dc59a9373f02e89798
dusong7 <dusong7@hotmail.com>
2017-05-02 11:58:08 +0800

    Update FIFO_.cpp

--- FIFO_.cpp
+++ FIFO_.cpp
@@ -1,90 +1,15 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <string.h>
-
-int value()
-{
-    const char *fifo_name = "/tmp/my_fifo";
-    int pipe_fd = -1;
-    int data_fd = -1;
-    int res = 0;
-    int open_mode = O_RDONLY;
-    char buffer[3 + 1];
-    int bytes_read = 0;
-    int bytes_write = 0;
-
-    memset(buffer, '\0', sizeof(buffer));
-
-    printf("Process %d opening FIFO O_RDONLY\n", getpid());
-    pipe_fd = open(fifo_name, open_mode);
-    data_fd = open("DataFormFIFO.txt", O_WRONLY|O_CREAT, 0644);
-
-
-    if (data_fd == -1)
-    {
-        fprintf(stderr, "Open file[DataFormFIFO.txt] failed\n");
-        close(pipe_fd);
-        return -1;
-    }
-
-    printf("Process %d result %d\n",getpid(), pipe_fd);
-    if(pipe_fd != -1)
-    {
-        do
-        {
-            res = read(pipe_fd, buffer, PIPE_BUF);
-            bytes_write = write(data_fd, buffer, res);
-            bytes_read += res;
-        }while(res > 0);
-
-        close(pipe_fd);
-        close(data_fd);
-    }
-    else
-        exit(EXIT_FAILURE);
-//    printf("%s",buffer);
-    printf("Process %d finished, %d bytes read\n", getpid(), bytes_read);
-    if (buffer[0] != '\0')
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-//    exit(EXIT_SUCCESS);
-}
-
-int main()
-{
-
-    while(1)
-    {
-        int num = value();
-        printf("%d_", num);
-        usleep(1000*1000*1);
-    }
-
-}
-
-
-//Ser
-
-#include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+#include <iostream>
 
-int writeFIFO(int i)
+int writeFIFO(int in)
 {
     const char *fifo_name = "/tmp/my_fifo";
     int pipe_fd = -1;
@@ -107,6 +32,12 @@ int writeFIFO(int i)
         }
     }
 
+    if(in != -1)
+    {
+        std::ofstream file;
+        file.open("Data.txt");
+        file<<in;
+    }
     printf("Process %d opening FIFO O_WRONLY\n", getpid());
     pipe_fd = open(fifo_name, open_mode);
     printf("Process %d result %d\n", getpid(), pipe_fd);
@@ -156,7 +87,92 @@ int main()
 //        writeFIFO(1);
 //        usleep(1000*1000*1);
 //    }
-     writeFIFO(1);
+     int i = 0;
+     while(1)
+     {
+
+         writeFIFO(i%2);
+         i++;
+         usleep(1000*1000*1);
+     }
+
      return 0;
    // exit(EXIT_SUCCESS);
+}
+
+//Cli
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+
+int value()
+{
+    const char *fifo_name = "/tmp/my_fifo";
+    int pipe_fd = -1;
+    int data_fd = -1;
+    int res = 0;
+    int open_mode = O_RDONLY;
+    char buffer[3 + 1];
+    int bytes_read = 0;
+    int bytes_write = 0;
+
+
+
+    memset(buffer, '\0', sizeof(buffer));
+
+    printf("Process %d opening FIFO O_RDONLY\n", getpid());
+    pipe_fd = open(fifo_name, open_mode);
+    data_fd = open("DataFormFIFO.txt", O_WRONLY|O_CREAT, 0644);
+
+
+    if (data_fd == -1)
+    {
+        fprintf(stderr, "Open file[DataFormFIFO.txt] failed\n");
+        close(pipe_fd);
+        return -1;
+    }
+
+    printf("Process %d result %d\n",getpid(), pipe_fd);
+    if(pipe_fd != -1)
+    {
+        do
+        {
+            res = read(pipe_fd, buffer, PIPE_BUF);
+            bytes_write = write(data_fd, buffer, res);
+            bytes_read += res;
+        }while(res > 0);
+
+        close(pipe_fd);
+        close(data_fd);
+    }
+    else
+        exit(EXIT_FAILURE);
+//    printf("%s",buffer);
+    printf("Process %d finished, %d bytes read\n", getpid(), bytes_read);
+    if (buffer[0] == '1')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+//    exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+
+    while(1)
+    {
+        int num = value();
+        printf("%d_", num);
+        usleep(1000*1000*1);
+    }
+
 }
