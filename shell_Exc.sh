2f0a3f95473dd38979dcaeed709ac13fb2825ccf
dusong7 <dusong7@hotmail.com>
2017-05-04 10:33:22 +0800

    Update shell_Exc.sh

--- shell_Exc.sh
+++ shell_Exc.sh
@@ -1,2 +1,10 @@
- system("cd /home/admini/Desktop/ && ls");  //
+ 
+ 
+ system("cd /home/admini/Desktop/ && ls");  ##combine
+ ##combine
  "gnome-terminal -x sh -c '%s ; cat' & gnome-terminal -x sh -c '%s ; cat' & gnome-terminal -x sh -c '%s ; cat'", argv[1],argv[2],argv[3]);
+
+
+
+
+
