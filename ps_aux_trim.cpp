aef21e7683c154749a3717d7117275b63df41e12
dusong7 <dusong7@hotmail.com>
2017-05-04 10:05:21 +0800

    Update ps_aux_trim.cpp

--- ps_aux_trim.cpp
+++ ps_aux_trim.cpp
@@ -104,3 +104,120 @@ int main()
 
     return 0;
 }
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include <zconf.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+bool  isModify()
+{
+    //
+    FILE *file;
+    file = fopen("file","r");
+    char str[2];
+    fread(str,2,2,file);
+
+    printf("%c",str[0]);
+
+    return false;
+}
+
+bool fileWrite(bool bl)
+{
+    FILE *file;
+    char *string1;
+    if (bl)
+    {
+//        string1 = "1";
+        memset(string1,0,0);
+        file = fopen("1","w");
+        if (access("file",0) != -1)
+        {
+            remove("file");
+        }
+    }
+    else
+    {
+//        string1 = "0";
+        //memset(string1,0,1);
+        file = fopen("1","w");
+        //fwrite("0",2,2,file);
+        remove("file");
+    }
+//    FILE *file;
+//    file = fopen("file","w");
+//    fwrite(string1,1,1,file);
+    fclose(file);
+}
+
+void trim(string &s)
+{
+
+    if( !s.empty() )
+    {
+        s.erase(0,s.find_first_not_of(" "));
+        s.erase(s.find_last_not_of(" ") + 1);
+    }
+
+}
+
+int main()
+{
+    //
+   // fileWrite(false);
+//    char *str1;
+//    char* str = gets(str1);
+//    printf("%s", str);
+//    string string2;
+//    system("ps -aux | grep 'xterm -T ProcessCmu1' > file");
+//    ifstream infile;
+//    infile.open("file");
+////    char *string1;
+//
+//    while(getline(string2,infile))
+//    {
+//        std::cout<<string2;
+//    }
+//    ofstream out;
+    ifstream in;
+
+//    out.open("psOut");
+//    out<<"Hello";
+//    out.close();
+    system("ps -aux | grep ProcessCmu1 >psOut");
+    in.open("psOut");
+    string string1;
+    getline(in,string1);
+    in.close();
+
+    //cout<<string1<<endl;
+    int i = string1.find('.');
+    string string2;
+    string2 = string1.substr(0,i-3);
+    int j = string2.find(' ');
+    string2 = string2.substr(j);
+    trim(string2);
+    cout<<string2<<endl;
+    string1 = "kill " + string2.substr(0,i-1);
+    cout<<string1;
+    const char *command = string1.c_str();
+//    //cout<<j;
+////    const string command = "kill "+string2;
+    system(command);
+// const  char * command = "kill " + string2;
+    //
+
+    system("ls;cd /home");
+
+    return 0;
+}
