8886c22281d13b89628d972003f385a5adab7b11
dusong7 <dusong7@hotmail.com>
2017-05-05 08:33:55 +0800

    Update shell_Exc.sh

--- shell_Exc.sh
+++ shell_Exc.sh
@@ -4,7 +4,7 @@
  ##combine
  "gnome-terminal -x sh -c '%s ; cat' & gnome-terminal -x sh -c '%s ; cat' & gnome-terminal -x sh -c '%s ; cat'", argv[1],argv[2],argv[3]);
 
-
+ps -aux == ps -pe  #shoe all process
 
 
 
