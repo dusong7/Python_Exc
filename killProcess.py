b536f0cbac4965de9cb1c2dd3d31b53c0023c3ba
dusong7 <dusong7@hotmail.com>
2017-05-05 09:40:56 +0800

    Update killProcess.py

--- killProcess.py
+++ killProcess.py
@@ -5,7 +5,13 @@ import os
 p = subprocess.Popen(['ps','-A'],stdout=subprocess.PIPE)
 out,err = p.communicate()
 
+curPPid = os.getppid() #parent pid
+
 for line in out.splitlines():
     if 'bash' in line:
         pid = int(line.split(None,1)[0])
-        os.kill(pid,signal.SIGKILL)
+	if(pid != curPPid):
+        	os.kill(pid,signal.SIGKILL)
+
+os.system("gnome-terminal -e ./fifo")
+os.kill(curPPid,signal.SIGKILL)
