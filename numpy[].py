5104a0621e8fd9f9608bd93ec240aa6316dcb92c
dusong7 <dusong7@hotmail.com>
2017-05-21 15:48:18 +0800

    Update numpy[].py

--- numpy[].py
+++ numpy[].py
@@ -1,3 +1,4 @@
+>>> import numpy as np
 >>> z = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 >>> z
 array([[ 1,  2,  3,  4],
@@ -37,3 +38,43 @@ array([[ 6],
 >>> z[1:-1,1:3]
 array([[ 6,  7],
        [10, 11]])
+>>> z
+array([[ 1,  2,  3,  4],
+       [ 5,  6,  7,  8],
+       [ 9, 10, 11, 12],
+       [13, 14, 15, 16]])
+>>> z[,1]
+  File "<stdin>", line 1
+    z[,1]
+      ^
+SyntaxError: invalid syntax
+>>> z[,1:]
+  File "<stdin>", line 1
+    z[,1:]
+      ^
+SyntaxError: invalid syntax
+>>> z[0,1:]
+array([2, 3, 4])
+>>> z[0]
+array([1, 2, 3, 4])
+>>> z[,]
+  File "<stdin>", line 1
+    z[,]
+      ^
+SyntaxError: invalid syntax
+>>> z[:,1]
+array([ 2,  6, 10, 14])
+>>> z[:]
+array([[ 1,  2,  3,  4],
+       [ 5,  6,  7,  8],
+       [ 9, 10, 11, 12],
+       [13, 14, 15, 16]])
+>>> z[:,0]  ## shuchu 列
+array([ 1,  5,  9, 13])
+>>> 
+
+
+
+
+##[x,y] x 行，y 列[m:n, p:q] 可用负数表示，当前行数减去总行数
+##
