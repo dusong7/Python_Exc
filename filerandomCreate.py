7e9abd6b3bf91713222cda4f2ca4dd2b20d7a8c5
dusong7 <dusong7@hotmail.com>
2017-05-24 17:30:49 +0800

    Update filerandomCreate.py

--- filerandomCreate.py
+++ filerandomCreate.py
@@ -1,6 +1,22 @@
+import io
+import sys
+import os
 import random
+import shutil
+table = "123456789"
 
-table = "1234567890"
-for i in range(100):
-  list = random.sample(table,10)
-  print("".join(list))
+# print(os.listdir("\list1"))
+
+list1 = []
+list2 = []
+for i in range(1000):
+    list = random.sample(table,3)
+    open("C://list1//" + "".join(list), 'w')
+    list1.append("C://list1//" + "".join(list))
+
+print(os.listdir("C://list1"))
+
+for i in range(1000):
+    list = random.sample(table,3)
+    open("C://list2//" + "".join(list), 'w')
+    list1.append("C://list2//" + "".join(list))
