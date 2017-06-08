613ccd8f722c8fdd900472539b66032c8e187538
dusong7 <dusong7@hotmail.com>
2017-06-08 10:19:27 +0800

    Create iFly.py

--- iFly.py
+++ iFly.py
@@ -4,8 +4,8 @@ while True:
     t = serial.Serial('/dev/ttyUSB0', 115200, 8, parity=serial.PARITY_NONE, stopbits=1)
 
     while True:
-        if t.read(6):
-            print(t.read(17))
+        if t.read(6):  ##\n\r
+            print(t.read(17))##weak up. angle
             # print(t.read(11))
             t.close()
             break;
