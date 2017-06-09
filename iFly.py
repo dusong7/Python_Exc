1c23fcc9f85e6d9a4800779e7b9806901cf007c4
dusong7 <dusong7@hotmail.com>
2017-06-09 09:58:45 +0800

    Create iFly.py

--- iFly.py
+++ iFly.py
@@ -1,5 +1,16 @@
 import serial
 import pyaudio
+import time
+import pygame
+file= "iflytek01.wav"
+pygame.mixer.init()
+print("播放音乐1")
+track = pygame.mixer.music.load(file)
+
+pygame.mixer.music.play()
+time.sleep(2)
+pygame.mixer.music.stop()
+
 ##sudo apt-get install python-pyaudio python3-pyaudio
 while True:
     t = serial.Serial('/dev/ttyUSB0', 115200, 8, parity=serial.PARITY_NONE, stopbits=1)
