967d9b00c3c33f4f8bb3bc1957ed32200075f32d
dusong7 <dusong7@hotmail.com>
2017-07-01 13:12:00 +0800

    Create pyConvert.py

---
+++ pyConvert.py
@@ -0,0 +1,15 @@
+To convert a string to bytes.
+
+data = ""           #string
+data = "".encode()  #bytes
+data = b""          #bytes
+1
+2
+3
+1
+2
+3
+To convert bytes to a String.
+data = b""          #bytes
+data = b"".decode() #string
+data = str(b"")     #string
