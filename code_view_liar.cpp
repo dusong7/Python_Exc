d76d63f046a6aed4f12ebb28679d7ce562030656
dusong7 <dusong7@hotmail.com>
2017-08-10 17:05:25 +0800

    Update code_view_liar.cpp

--- code_view_liar.cpp
+++ code_view_liar.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 
-int number(int N, int array[10])
+int number(int N, int *array)
 {
 	//
 	int ncount[10] = {0};
@@ -16,7 +16,7 @@ int number(int N, int array[10])
 	{
 		for (size_t j = 0; j < N; j++)
 		{
-			if (array[i] == j)
+			if (*array++ == j)
 			{
 				ncount[j]++;
 			}
@@ -39,13 +39,14 @@ int main(void)
 {
 	int array[10] = { 0 };
 	int N;
+	int *array1;
 	cin >> N;
-
+	array1 = (int *)malloc(N * sizeof(int));
 	for (size_t i = 0; i < N; i++)
 	{
-		cin >> array[i];
+		cin >> *array1++;
 	}
 
-	printf("%d\n", number(N, array));
+	printf("%d\n", number(N, array1));
 	return 0;
 }
