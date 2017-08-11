a0441c0c93f0734b9f422ed1d595baa167c76aed
dusong7 <dusong7@hotmail.com>
2017-08-11 09:25:25 +0800

    Update code_view_liar.cpp

--- code_view_liar.cpp
+++ code_view_liar.cpp
@@ -132,3 +132,79 @@ int main(void)
 }
 
 
+/**
+//说谎者问题
+//通过计算求出 说谎者数 + 未说谎者数 = 总数
+//Create By Du Songchang
+//mail: dusong7@hotmail.com
+//IDE: Code::Blocks
+//compiler: gcc 4.9.2
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int liarCount(int N, int *array)
+{
+	int nCount[10] = { 0 };
+	//
+	//统计出每组说谎者数量
+	//e.g (0,1) 有0个说谎者的有1个人
+	//算法复杂度为 O(n2)
+	//STL 下可以用map来解决， 复杂度 O(logN)
+
+	for (int i = 0; i < N; i++)
+	{
+		int value = *array++;
+		for (int j = 0; j < N; j++)
+		{
+			if (value == j)
+			{
+				nCount[j]++;
+			}
+		}
+	}
+
+	//计算出当列说谎者的数量+未说谎者的数量 = N的最小值
+	//e.g (1,1),  1+1 == N ?
+	//
+	//
+	for (int i = 0; i <= N; i++)
+	{
+		if (nCount[i] + i == N)
+		{
+			return i;
+			break;
+		}
+	}
+
+	return -1;
+}
+
+int main(void)
+{
+	int N;  //总人数
+	int *arrayOri; //说谎者数据
+	scanf("%d", &N);
+	//N 值输入可能为负值
+	//malloc 可能失败
+	//
+	if (N>0)
+	{
+		if (arrayOri = (int *)malloc(N * sizeof(int)))
+		{
+			for (int i = 0; i < N; i++)
+			{
+				scanf("%d", &arrayOri[i]);
+			}
+			printf("%d\n", liarCount(N, arrayOri));
+		}
+	}
+	else
+	{
+		//
+	}
+
+	return 0;
+}
+
